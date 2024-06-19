e5fafd03eb93acea3e380a53791b157790fdf7b5
Zay Min Paing <zayminpaing.ptn@gmail.com>
2024-06-20 01:37:29 +0630

    Allow origins v2

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -14,7 +14,13 @@ public class ECommerceApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
